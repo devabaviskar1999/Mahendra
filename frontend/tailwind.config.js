335e138353a8de6d66555f76fa27ce6756769e8a
devendra baviskar <devbaviskar1999@gmail.com>
2025-05-20 16:57:34 +0530

    message

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // scans all JS/TS/React files in src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
