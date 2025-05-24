ea887db2d0aee0a184c6f7959e74d491b84cd814
devendra baviskar <devbaviskar1999@gmail.com>
2025-05-24 13:18:19 +0530

    add bg-black on rooms

--- frontend/src/pages/rooms/Rooms.tsx
+++ frontend/src/pages/rooms/Rooms.tsx
@@ -6,68 +6,68 @@ export default function Rooms() {
   const ThirdRoom = "https://i.ibb.co/wNJ5LB0Z/Third-room.jpg";
   return (
     <ScrollToTop>
-      <div
-        className="flex flex-col items-center w-full h-auto mt-0 bg-[#f2ebe5]
-    py-6 md:py-10"
-      >
-        <h1 className="text-3xl p-1 sm:text-4xl md:text-5xl lg:text-5xl font-semibold ">
-          Rooms
-        </h1>
-        <h2 className="font-serif text-sm text-center md:text-lg">
-          Experience the luxury of our{" "}
-          <span className="font-semibold">five</span> exclusive room categories,
-          tailored for your ultimate comfort.
-        </h2>
-        <RoomComponent
-          Img={Room}
-          category="Ac Suite"
-          rate={4200}
-          description='The image is still loading while the motion.div is animating in.
+        <div
+          className="flex flex-col items-center w-full h-auto mt-0 bg-black  lg:bg-[#f2ebe5]
+        py-6 md:py-10"
+        >
+          <h1 className="text-3xl p-1 sm:text-4xl md:text-5xl lg:text-5xl font-semibold text-white ">
+            Rooms
+          </h1>
+          <h2 className="font-serif text-sm text-center md:text-lg text-white">
+            Experience the luxury of our{" "}
+            <span className="font-semibold text-white">five</span> exclusive room
+            categories, tailored for your ultimate comfort.
+          </h2>
+          <RoomComponent
+            Img={Room}
+            category="Ac Suite"
+            rate={4200}
+            description='The image is still loading while the motion.div is animating in.
+          So the layout shifts mid-animation, making it feel like it"s "stuck" or stuttering — especially because the image has no defined height/width upfront.'
+            reviewCount={135}
+            rating={4.5}
+            bedType="King Size Bed"
+          />
+          <RoomComponent
+            Img="https://i.ibb.co/FkH9fLhb/Double-bed.jpg"
+            category="Ac Mini Suite"
+            rate={2600}
+            description='The image is still loading while the motion.div is animating in.
+          So the layout shifts mid-animation, making it feel like it"s "stuck" or stuttering — especially because the image has no defined height/width upfront.'
+            reviewCount={73}
+            rating={4.1}
+            bedType="Double Bed"
+          />
+          <RoomComponent
+            Img={ThirdRoom}
+            category="Ac Super Deluxe"
+            rate={2000}
+            description='The image is still loading while the motion.div is animating in.
 So the layout shifts mid-animation, making it feel like it"s "stuck" or stuttering — especially because the image has no defined height/width upfront.'
-          reviewCount={135}
-          rating={4.5}
-          bedType="King Size Bed"
-        />
-        <RoomComponent
-          Img="https://i.ibb.co/FkH9fLhb/Double-bed.jpg"
-          category="Ac Mini Suite"
-          rate={2600}
-          description='The image is still loading while the motion.div is animating in.
-So the layout shifts mid-animation, making it feel like it"s "stuck" or stuttering — especially because the image has no defined height/width upfront.'
-          reviewCount={73}
-          rating={4.1}
-          bedType="Double Bed"
-        />
-        <RoomComponent
-          Img={ThirdRoom}
-          category="Ac Super Deluxe"
-          rate={2000}
-          description='The image is still loading while the motion.div is animating in.
-So the layout shifts mid-animation, making it feel like it"s "stuck" or stuttering — especially because the image has no defined height/width upfront.'
-          reviewCount={97}
-          rating={4.8}
-          bedType="Triple Bed"
-        />
-        <RoomComponent
-          Img={ThirdRoom}
-          category="Ac Deluxe"
-          rate={1350}
-          description='The image is still loading while the motion.div is animating in.
+            reviewCount={97}
+            rating={4.8}
+            bedType="Triple Bed"
+          />
+          <RoomComponent
+            Img={ThirdRoom}
+            category="Ac Deluxe"
+            rate={1350}
+            description='The image is still loading while the motion.div is animating in.
+          So the layout shifts mid-animation, making it feel like it"s "stuck" or stuttering — especially because the image has no defined height/width upfront.'
+            reviewCount={97}
+            rating={4.8}
+            bedType="Triple Bed"
+          />
+          <RoomComponent
+            Img={ThirdRoom}
+            category="Ac Dormatory(6 Pax)"
+            rate={3000}
+            description='The image is still loading while the motion.div is animating in.
         So the layout shifts mid-animation, making it feel like it"s "stuck" or stuttering — especially because the image has no defined height/width upfront.'
-          reviewCount={97}
-          rating={4.8}
-          bedType="Triple Bed"
-        />
-        <RoomComponent
-          Img={ThirdRoom}
-          category="Ac Dormatory(6 Pax)"
-          rate={3000}
-          description='The image is still loading while the motion.div is animating in.
-        So the layout shifts mid-animation, making it feel like it"s "stuck" or stuttering — especially because the image has no defined height/width upfront.'
-          reviewCount={97}
-          rating={4.8}
-          bedType="Triple Bed"
-        />
+            reviewCount={97}
+            rating={4.8}
+            bedType="Triple Bed"
+          />
       </div>
     </ScrollToTop>
   );
